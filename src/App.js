4252e07167b5f5ad66c0af71b25e323a53e19ad5
Mykola Dzoban <nick.dzoban@gmail.com>
2023-08-02 14:34:18 +0300

    react 17

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-// import logo from './logo.svg';
 import './App.css'; 
+import { Contacts } from './components/Contacts';
+// import { Filters } from './components/Filters';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Here will be contacts...
+          Контакти
         </p>
       </header>
+      <main>
+        <Contacts />
+      </main>
+      
     </div>
   );
 }
